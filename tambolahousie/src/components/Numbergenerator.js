5729692c8d08f380efac377e19b610d991df9f48
Gayathri Mantha <gayathrimantha16@gmail.com>
2023-10-07 19:38:21 +0530

    //commit

--- tambolahousie/src/components/Numbergenerator.js
+++ tambolahousie/src/components/Numbergenerator.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import "../css/Numbergenerator.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee, faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 const Numbergenerator = () => {
   const [isIconHovered, setIsIconHovered] = useState(false);
-  const numbersArray = Array.from({ length: 90 }, (_, index) => index + 1);
-  console.log(numbersArray);
+  const [numbersArray, setNumbersArray] = useState(
+    Array.from({ length: 90 }, (_, index) => index + 1)
+  );
+  const [generatedNumbers, setGeneratedNumbers] = useState(new Set());
+  const [currentNumber, setCurrentNumber] = useState(null);
 
   const handleMouseEnter = () => {
     setIsIconHovered(true);
@@ -14,6 +17,21 @@ const Numbergenerator = () => {
 
   const handleMouseLeave = () => {
     setIsIconHovered(false);
+  };
+
+  const generateRandomNumber = () => {
+    if (numbersArray.length === 0) {
+      // All numbers have been generated
+      setCurrentNumber(null);
+    } else {
+      const randomIndex = Math.floor(Math.random() * numbersArray.length);
+      const selectedNumber = numbersArray[randomIndex];
+      const updatedNumbersArray = [...numbersArray];
+      updatedNumbersArray.splice(randomIndex, 1);
+
+      setCurrentNumber(selectedNumber);
+      setNumbersArray(updatedNumbersArray);
+    }
   };
 
   return (
@@ -25,25 +43,49 @@ const Numbergenerator = () => {
         <div className="numbersDiv">
           <div className="numbersDisplay">
             {numbersArray.map((number) => (
-              <div className="boxStyle">
-                <div className="numberStyle">{number}</div>
+              <div
+                className={
+                  generatedNumbers.has(number)
+                    ? "boxStyleGenerated"
+                    : "boxStyle"
+                }
+                key={number}
+              >
+                <div
+                  className={
+                    generatedNumbers.has(number)
+                      ? "numberStyleGenerated"
+                      : "numberStyle"
+                  }
+                >
+                  {number}
+                </div>
               </div>
             ))}
           </div>
         </div>
-        <div className="settingsContent">
-          <div
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            className={isIconHovered ? "hoveredIcon" : "playButton"}
-          >
-            <FontAwesomeIcon
-              icon={faPlay}
-              className={isIconHovered ? "hoveredIconStyle" : "iconStyle"}
-              size="2xl"
-            />
-          </div>
+        <div
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          className={isIconHovered ? "hoveredIcon" : "playButton"}
+          onClick={generateRandomNumber}
+        >
+          <FontAwesomeIcon
+            icon={faPlay}
+            className={isIconHovered ? "hoveredIconStyle" : "iconStyle"}
+            size="2xl"
+          />
         </div>
+        <div
+          className={isIconHovered ? "hoveredPlayTextStyle" : "playTextStyle"}
+        >
+          PLAY
+        </div>
+        {currentNumber && (
+          <div className="generatedNumber">
+            <div className="numberStyleCurrent"> {currentNumber}</div>
+          </div>
+        )}
       </div>
     </div>
   );
