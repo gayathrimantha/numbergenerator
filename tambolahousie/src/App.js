0a56409bfc9dcbbd219bc6cc91d39f7fb6d0aa4c
Gayathri Mantha <gayathrimantha16@gmail.com>
2023-10-07 15:08:32 +0530

    initial commit

---
+++ tambolahousie/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import Numbergenerator from "./components/Numbergenerator";
+
+function App() {
+  return (
+    <div className="App">
+      <Numbergenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ tambolahousie/src/components/Numbergenerator.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../css/Numbergenerator.css";
+
+const Numbergenerator = () => {
+  return (
+    <div className="mainContainer">
+      <div className="headerTitle">
+        NUMBER GENERATOR FOR TAMBOLA, HOUSIE AND BINGO
+      </div>
+      <div className="numbersDivParent">
+        <div className="numbersDiv">
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Numbergenerator;
